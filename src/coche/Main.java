eb0d75b67fb977ba68a1f56c936cafd922915a19
JLV_a <JLV_a@LAPTOP-S6V4FGMQ>
2021-05-11 19:43:19 +0200

    estado inicial
    Encapsulación de campos de la clase Coche

---
+++ src/coche/Main.java
@@ -0,0 +1,45 @@
+
+package coche;
+
+/**
+ *
+ * @author Fran
+ */
+public class Main {
+    
+      public static void main(String[] args) {
+        Coche miCoche;
+        int stockActual;
+        
+        miCoche = new Coche("Opel",12000,500);
+        operatoriaVenta(miCoche);
+        
+        operatoriaCompra(miCoche);
+        stockActual = miCoche.obtenerStock();
+        System.out.println("El stock actual es"+ stockActual );
+    }
+
+    private static void operatoriaCompra(Coche miCoche) {
+        try
+        {
+            System.out.println("Compra de Coches");
+            miCoche.comprar(500);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+
+    private static void operatoriaVenta(Coche miCoche) {
+        try
+        {
+            System.out.println("Venta de Coches");
+            miCoche.vender(300);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al vender");
+        }
+    }
+
+}
+    
